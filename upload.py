a05e6acc0662dfcd9b7c1a1b77654685336ce97f
m1yag1 <8730430+m1yag1@users.noreply.github.com>
2023-12-22 23:28:54 -0600

    Updated template and added content-type to s3 upload

--- upload.py
+++ upload.py
@@ -1,4 +1,3 @@
-
 import os
 
 import boto3
@@ -6,11 +5,17 @@ import boto3
 
 def upload_site(build_path):
     # Connect to S3
-    s3 = boto3.client('s3', region_name="us-east-1")
+    s3 = boto3.client("s3", region_name="us-east-1")
 
     # Upload File
-    bucket_name = 'stonksfeed'
-    s3.upload_file(build_path + "/index.html", bucket_name, "index.html")
+    bucket_name = "stonksfeed"
+    content_type = "text/html"
+    s3.upload_file(
+        build_path + "/index.html",
+        bucket_name,
+        "index.html",
+        ExtraArgs={"ContentType": content_type},
+    )
 
 
 if __name__ == "__main__":
