96c7d7e087c12ae7326eadaff997211b845bfa07
m1yag1 <8730430+m1yag1@users.noreply.github.com>
2026-01-16 17:37:00 -0600

    Add new RSS feeds for broader coverage
    - Motley Fool Investing
    - BizToc Business News (aggregator ~200 articles/fetch)
    - PR Newswire Financial Services
    - GlobeNewswire Earnings
    
    Total articles per fetch cycle: ~430 (up from ~140)

--- packages/stonksfeed/src/stonksfeed/config.py
+++ packages/stonksfeed/src/stonksfeed/config.py
@@ -12,6 +12,41 @@ RSS_FEEDS = [
         "feed_title": "Real-time Headlines",
         "rss_url": "https://feeds.content.dowjones.io/public/rss/mw_realtimeheadlines",
     },
+    {
+        "publisher": "Seeking Alpha",
+        "feed_title": "Latest Articles",
+        "rss_url": "https://seekingalpha.com/feed.xml",
+    },
+    {
+        "publisher": "CNBC",
+        "feed_title": "Markets",
+        "rss_url": "https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=20910258",
+    },
+    {
+        "publisher": "Investing.com",
+        "feed_title": "News",
+        "rss_url": "https://www.investing.com/rss/news.rss",
+    },
+    {
+        "publisher": "Motley Fool",
+        "feed_title": "Investing",
+        "rss_url": "https://www.fool.com/feeds/index.aspx",
+    },
+    {
+        "publisher": "BizToc",
+        "feed_title": "Business News",
+        "rss_url": "https://biztoc.com/feed",
+    },
+    {
+        "publisher": "PR Newswire",
+        "feed_title": "Financial Services",
+        "rss_url": "https://www.prnewswire.com/rss/financial-services-latest-news/financial-services-latest-news-list.rss",
+    },
+    {
+        "publisher": "GlobeNewswire",
+        "feed_title": "Earnings",
+        "rss_url": "https://globenewswire.com/RssFeed/subjectcode/13-Earnings/feedTitle/GlobeNewswire%20-%20Earnings",
+    },
 ]
 
 # Silicon Investor forums to scrape
